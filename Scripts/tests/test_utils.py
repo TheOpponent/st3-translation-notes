045f06840d5bb36c0192332719fa408f71c2f0bc
Gong Xian <theo@minuspoint.com>
2022-07-02 20:36:46 -0400

    Update linebreak test

--- Scripts/tests/test_utils.py
+++ Scripts/tests/test_utils.py
@@ -4,8 +4,8 @@ def test_linebreak():
 
     # Test line breaking.
     test_string1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod labore et dolor magna aliqua."
-    assert utils.linebreak(test_string1) == (r"Lorem ipsum dolor sit amet,\consectetur adipiscing elit, sed do\eiusmod labore et dolor magna aliqua.",3)
+    assert utils.linebreak(test_string1) == (r"Lorem ipsum dolor sit amet,\consectetur adipiscing elit, sed do\eiusmod labore et dolor magna aliqua.",3,0)
 
-    # Test a forced line break, which will cause overflow.
+    # Test a forced line break, which will cause overflow and generate 1 warning.
     test_string2 = r"Lorem ipsum//dolor sit amet, consectetur adipiscing elit, sed do eiusmod labore et dolor magna aliqua."
-    assert utils.linebreak(test_string2) == (r"Lorem ipsum\dolor sit amet, consectetur\adipiscing elit, sed do eiusmod\labore et dolor magna aliqua.",4)
+    assert utils.linebreak(test_string2) == (r"Lorem ipsum\dolor sit amet, consectetur\adipiscing elit, sed do eiusmod\labore et dolor magna aliqua.",4,1)
